4c91bafdbf7da11be6f8e973d130c016078484a0
Rugilė <rugilena@gmail.com>
2015-04-01 20:26:10 +0300

    Delete setup.py

--- entrepreneur/spiders/setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('/home/eksponente/Downloads/cx_Freeze-4.3.3/')
-from cx_Freeze import setup, Executable
-
-setup(name='Article Scraper',
-	version='1.0',
-	description='Use this program to scrape articles from entrepreneur.com',
-	executables = [Executable("article_spider.py")])
