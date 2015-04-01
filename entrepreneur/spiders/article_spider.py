d1c359c4ce02073643b47c7cd76483cac40b35a4
Rugilė <rugilena@gmail.com>
2015-04-01 20:28:51 +0300

    Update article_spider.py

--- entrepreneur/spiders/article_spider.py
+++ entrepreneur/spiders/article_spider.py
@@ -49,10 +49,6 @@ class ArticleSpider(scrapy.Spider):
 			"Starting a Business"
 		]
 
-		# request = Request("http://www.entrepreneur.com/article/234070", callback=self.scrap)
-		# request.meta['filename'] = "Topic"
-		# request.meta['directory'] = directory
-		# return request
 		for i in range(8):
 			request = Request(topic_urls[i], callback=self.scrap_pages)
 			request.meta['filename'] = topics[i]
@@ -83,47 +79,28 @@ class ArticleSpider(scrapy.Spider):
 		globvar = globvar + 1
 		global sheet
 		item['sheet'] = sheet
-		# dir = os.path.dirname(__file__)
-		# directory = os.path.join(dir, 'articles')
-		#
-		# text_file = codecs.open(directory + '/' + str(globvar) + ".doc", "w", encoding="utf-8")
 		text = response.xpath('//h1[contains(@itemprop, "headline")]/text()').extract()
 		if text:
 			item['heading'] = text[0].strip()
 		else:
 			item['heading'] = ""
-			# geras = text[0].strip()
-			# sheet.write(globvar, 0, geras.strip())
 		item['topic'] = response.meta['filename']
-		# sheet.write(globvar, 1, response.meta['filename'])
 		t = response.xpath('//div[contains(@class, "article-body")]/time/text() | //div[contains(@class, "block")]/time/text()').extract()
 		if t:
 			item['date'] = t[0].strip()
 		else:
 			item['date'] = ""
-			# sheet.write(globvar, 2, t[0].decode('utf-8').strip() )
 		item['number'] = globvar
-		# sheet.write(globvar, 4, str(globvar) + ".doc")
 		item['url'] = response.url
-		# sheet.write(globvar, 5, response.url)
-		# sheet.write(globvar, 6, "")
-		# text_file.write(text[0].strip())
-		# text_file.write('\n\n')
 
 		text = response.xpath('//div[contains(@class, "content-block")]').extract()
 		item['body'] = text[0]
-		# h = html2text.HTML2Text()
-		# h.ignore_images = True
-		# text_file.write(h.handle(text[0]))
-		# text_file.close()
 		author = response.xpath('//a[contains(@rel, "author")]/div/text()').extract()
 		if author:
 			item['author'] = author[0].strip()
 		else:
 			item['author'] = ""
-			# sheet.write(globvar, 3, author[0].strip())
 		global workbook
 		item['workbook'] = workbook
 		item['directory'] = response.meta['directory']
-		# workbook.save(directory + "/reference.xls")
 		return item
